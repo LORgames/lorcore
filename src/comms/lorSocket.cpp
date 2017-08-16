7d470cc04437fcbc0a6849af77a5e431bdfa9beb
Paul Fox <p.fox@lorgames.com>
2017-08-16 16:35:06 +1000

    Added the ability to set a socket as a server

--- src/comms/lorSocket.cpp
+++ src/comms/lorSocket.cpp
@@ -1,6 +1,12 @@
 #include "lorSocket.h"
 #include "lorCore.h"
 
+#include "mbedtls/net.h"
+#include "mbedtls/ssl.h"
+#include "mbedtls/entropy.h"
+#include "mbedtls/ctr_drbg.h"
+#include "mbedtls/debug.h"
+
 #ifdef _MSC_VER
 #include <winsock2.h>
 #include <Ws2tcpip.h>
@@ -40,13 +46,17 @@ int inet_pton(int af, const char *src, void *dst)
 }
 #endif
 
+#ifndef INVALID_SOCKET
+typedef int SOCKET;
+#define INVALID_SOCKET  (SOCKET)(~0)
+#define SOCKET_ERROR            (-1)
+#endif //INVALID_SOCKET
+
 struct lorSocket
 {
-#ifdef _MSC_VER
   SOCKET sockID;
-#else
-  int32_t sockID;
-#endif //_MSC_VER
+  bool isServer;
+  bool isSecure;
 };
 
 bool lorSocket_InitSystem()
@@ -68,105 +78,152 @@ bool lorSocket_DeinitSystem()
 #endif
 }
 
-bool lorSocket_IsValidSocket(lorSocket *pSocket)
+int lorSocket_GetErrorCode()
 {
 #ifdef _MSC_VER
-  return (pSocket->sockID != INVALID_SOCKET);
+  return WSAGetLastError();
 #else
-  return (pSocket->sockID > 0);
-#endif //_MSC_VER
+  return errno;
+#endif
 }
 
-bool lorSocket_Connect(lorSocket **ppSocket, char *serverName, uint32_t port)
+bool lorSocket_IsValidSocket(lorSocket *pSocket)
 {
-  lorLog("Socket connection to %s:%d", serverName, port);
-  bool retVal = false;
+  return (pSocket->sockID != INVALID_SOCKET);
+}
+
+bool lorSocket_OpenSocket(lorSocket **ppSocket, char *pAddress, uint32_t port, lorSocketConnectionFlags flags)
+{
+  lorLog("Socket connection to %s:%d", pAddress, port);
+  bool openSuccess = false;
 
   *ppSocket = lorAllocType(lorSocket, 1);
   lorSocket *pSocket = *ppSocket;
 
-  pSocket->sockID = socket(AF_INET, SOCK_STREAM, 0);
+  bool isServer = (flags & lSCFIsServer) > 0;
+  //bool isSecure = (flags & lSCFUseTLS) > 0;
+
+  addrinfo *pOutAddr;
+
+  addrinfo hints;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_flags = AI_PASSIVE;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_protocol = IPPROTO_TCP;
+
+  char buffer[6];
+  lorSprintf(buffer, sizeof(buffer), "%d", port);
+  int retVal = getaddrinfo(pAddress, buffer, &hints, &pOutAddr);
+  if (retVal != 0)
+    goto epilogue;
+
+  pSocket->sockID = socket(pOutAddr->ai_family, pOutAddr->ai_socktype, (int)pOutAddr->ai_addrlen);
 
   if (!lorSocket_IsValidSocket(pSocket))
     goto epilogue;
 
-  in_addr dest;
-
-  if (inet_pton(AF_INET, serverName, &dest) == 1)
+  if (isServer)
   {
-    sockaddr_in clientService;
-    clientService.sin_family = AF_INET;
-    clientService.sin_port = htons((uint16_t)port);
-    clientService.sin_addr = dest;
+    pSocket->isServer = true;
 
-    int socketResult = (int)connect(pSocket->sockID, (const sockaddr*)&clientService, sizeof(clientService));
+    retVal = bind(pSocket->sockID, pOutAddr->ai_addr, (int)pOutAddr->ai_addrlen);
+    if (retVal == SOCKET_ERROR)
+      goto epilogue;
 
-    if (socketResult == 0)
-    {
-      retVal = true;
-    }
-    else
-    {
-#ifdef _MSC_VER
-      lorLog("Socket Error: %d", WSAGetLastError());
-#else
-      lorLog("Socket Error: %d", errno);
-#endif //_MSC_VER
-    }
+    retVal = listen(pSocket->sockID, SOMAXCONN);
+    if (retVal == SOCKET_ERROR)
+      goto epilogue;
   }
+  else
+  {
+    retVal = connect(pSocket->sockID, pOutAddr->ai_addr, (int)pOutAddr->ai_addrlen);
+    if (retVal == SOCKET_ERROR)
+      goto epilogue;
+  }
+
+  openSuccess = true;
 
 epilogue:
-  if (!retVal)
-  {
-    lorFree(*ppSocket);
-    *ppSocket = nullptr;
-  }
+  if (pOutAddr != nullptr)
+    freeaddrinfo(pOutAddr);
 
-  lorLog("\t...Connection %s!", retVal?"Success":"Failed");
-  return retVal;
+  if (!openSuccess)
+    lorSocket_CloseSocket(&pSocket);
+
+  lorLog("\t...Connection %s!", openSuccess ? "Success" : "Failed");
+  return openSuccess;
 }
 
-bool lorSocket_Close(lorSocket **ppSocket)
+bool lorSocket_CloseSocket(lorSocket **ppSocket)
 {
   if (*ppSocket == nullptr)
     return true;
 
 #ifdef _MSC_VER
-  int status = shutdown((*ppSocket)->sockID, SD_BOTH);
-  if (status == 0)
-    status = closesocket((*ppSocket)->sockID);
+  closesocket((*ppSocket)->sockID);
 #else
-  int status = (int)shutdown((*ppSocket)->sockID, SHUT_RDWR);
-  if (status == 0)
-    status = (int)close((*ppSocket)->sockID);
+  close((*ppSocket)->sockID);
 #endif
 
   lorFree(*ppSocket);
   *ppSocket = nullptr;
 
-  return (status == 0);
+  return true;
 }
 
 bool lorSocket_SendData(lorSocket *pSocket, const uint8_t *pBytes, uint16_t totalBytes)
 {
+  if (pSocket == nullptr || pBytes == nullptr || totalBytes == 0)
+    return false;
+
   return (totalBytes != (int)send(pSocket->sockID, (const char*)pBytes, totalBytes, 0));
 }
 
-int lorSocket_ReceiveData(lorSocket *pSocket, uint8_t *pBytes, uint16_t bufferSize)
+int lorSocket_ReceiveData(lorSocket *pSocket, uint8_t *pBytes, uint16_t bufferSize, bool blockForever /*= false*/)
 {
-  struct timeval tv;
-  fd_set readfds;
+  if (pSocket == nullptr || pBytes == nullptr || bufferSize == 0 || pSocket->isServer)
+    return 0;
 
-  tv.tv_sec = 0;
-  tv.tv_usec = 500000;
+  if (!blockForever)
+  {
+    struct timeval tv;
+    fd_set readfds;
 
-  FD_ZERO(&readfds);
-  FD_SET(pSocket->sockID, &readfds);
+    tv.tv_sec = 0;
+    tv.tv_usec = 500000;
 
-  select(0, &readfds, nullptr, nullptr, &tv);
+    FD_ZERO(&readfds);
+    FD_SET(pSocket->sockID, &readfds);
 
-  if (FD_ISSET(pSocket->sockID, &readfds))
+    select(0, &readfds, nullptr, nullptr, &tv);
+
+    if (FD_ISSET(pSocket->sockID, &readfds))
+      return (int)recv(pSocket->sockID, (char*)pBytes, bufferSize, 0);
+  }
+  else
+  {
     return (int)recv(pSocket->sockID, (char*)pBytes, bufferSize, 0);
+  }
 
   return 0;
+}
+
+bool lorSocket_ServerAcceptClient(lorSocket *pServerSocket, lorSocket **ppClientSocket)
+{
+  if (ppClientSocket == nullptr || pServerSocket == nullptr || !pServerSocket->isServer)
+    return false;
+
+  *ppClientSocket = nullptr;
+
+  SOCKET clientSocket = accept(pServerSocket->sockID, nullptr, nullptr);
+
+  if (clientSocket != INVALID_SOCKET)
+  {
+    (*ppClientSocket) = lorAllocType(lorSocket, 1);
+    (*ppClientSocket)->sockID = clientSocket;
+    return true;
+  }
+
+  return false;
 }
--- src/comms/lorSocket.h
+++ src/comms/lorSocket.h
@@ -8,11 +8,21 @@ struct lorSocket;
 bool lorSocket_InitSystem();
 bool lorSocket_DeinitSystem();
 
-bool lorSocket_Connect(lorSocket **ppSocket, char *serverName, uint32_t port);
-bool lorSocket_Close(lorSocket **ppSocket);
+enum lorSocketConnectionFlags
+{
+  lSCFNone = 0,
+  lSCFIsServer = 1,
+  lSCFUseTLS = 2
+};
+inline lorSocketConnectionFlags operator |(lorSocketConnectionFlags a, lorSocketConnectionFlags b) { return (lorSocketConnectionFlags)(((int)a) | ((int)b)); }
+
+bool lorSocket_OpenSocket(lorSocket **ppSocket, char *pAddress, uint32_t port, lorSocketConnectionFlags flags = lSCFNone);
+bool lorSocket_CloseSocket(lorSocket **ppSocket);
 bool lorSocket_IsValidSocket(lorSocket *pSocket);
 
 bool lorSocket_SendData(lorSocket *pSocket, const uint8_t *pBytes, uint16_t totalBytes);
-int lorSocket_ReceiveData(lorSocket *pSocket, uint8_t *pBytes, uint16_t bufferSize);
+int lorSocket_ReceiveData(lorSocket *pSocket, uint8_t *pBytes, uint16_t bufferSize, bool blockForever = false);
+
+bool lorSocket_ServerAcceptClient(lorSocket *pServerSocket, lorSocket **ppClientSocket);
 
 #endif // LOR_SOCKET
