dfedefd241a529b50421b5bac7c6e7715cb6af7b
Paul Fox <p.fox@lorgames.com>
2018-01-23 23:03:01 +1000

    Fixed minor issue with time and added a *very* dodgy entropy source to wp8.1 & ws8.1

--- src/lorTime.cpp
+++ src/lorTime.cpp
@@ -5,5 +5,5 @@
 //Returns number of seconds since the start of January 1, 1970 @ UTC
 uint64_t lorTime_GetCurrentTimestamp()
 {
-  return std::chrono::system_clock::now().time_since_epoch().count() / 1000;
+  return std::chrono::system_clock::now().time_since_epoch().count() / std::chrono::system_clock::period::den;
 }
